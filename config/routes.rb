96023b90ff53e5ee1347497c678bcef873e9a823
andressamachado <andressa.pamachado@gmail.com>
2022-07-25 17:00:23 -0400

    WIP - adds user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # get '/', to: 'main#index'
   # get '', to: 'main#index'
   root to: 'main#index'
+
   get "about-us", to: "about#index", as: :about
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
