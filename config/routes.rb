64ad5c51af3d82900692883b8b63d28e2b37fd07
andressamachado <andressa.pamachado@gmail.com>
2022-07-25 10:27:17 -0400

    adds basic structure for main page - route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/', to: 'main#index'
+  # get '', to: 'main#index'
+  root to: 'main#index'
 end
