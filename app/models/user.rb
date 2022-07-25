1ed515b1976451d0e54b57d5df7d236489912a93
andressamachado <andressa.pamachado@gmail.com>
2022-07-25 14:26:58 -0400

    Adds validation to User rtable entry

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   # then take the password and run it through bcrypt to create a digest
   # and assign that digest for us
   has_secure_password
+
+  validates :email, presence: true, format: {
+    with: URI::MailTo::EMAIL_REGEXP,
+    message: 'invalid email address provided'
+  }
 end
