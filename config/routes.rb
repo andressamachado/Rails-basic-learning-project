e5016795fa4d68f07b88a9ef213a856cd3e1adfb
andressamachado <andressa.pamachado@gmail.com>
2022-07-29 14:33:48 -0400

    Adds Forgot Password feature

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   patch "password", to: "passwords#update"
 
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
 end
