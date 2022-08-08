9d4a03600b02121e4b9a11cba1b6dd835fcbc525
andressamachado <andressa.pamachado@gmail.com>
2022-08-08 11:33:38 -0400

    WIP - Tweet Model and page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
   has_many :twitter_accounts
+  has_many :tweets
   
   # will make sure the password and password_confirmation match,
   # then take the password and run it through bcrypt to create a digest
