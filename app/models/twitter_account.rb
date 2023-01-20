baf7f9809e5692ea859f284df7321f9ee5159909
andressamachado <andressa.pamachado@gmail.com>
2023-01-19 20:13:48 -0500

    updating credentials

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -6,6 +6,7 @@ class TwitterAccount < ApplicationRecord
 
   def client
     Twitter::REST::Client.new do |config|
+
       config.consumer_key        = Rails.application.credentials.dig(:twitter, :consumer_key)
       config.consumer_secret     = Rails.application.credentials.dig(:twitter, :consumer_secret)
       config.access_token        = token
